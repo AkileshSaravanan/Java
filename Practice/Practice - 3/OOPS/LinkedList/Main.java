fc03a79a81ff2e1b5f028edc70b6ac98e59028e7
AKILESH S <rmsakilesh23@gmail.com>
2023-07-29 18:05:17 +0530

    initial commit

---
+++ Practice/Practice - 3/OOPS/LinkedList/Main.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+    cars.addFirst("Mazda");
+    System.out.println(cars);
+    cars.addLast("Mazda");
+    System.out.println(cars);
+    cars.removeFirst();
+    System.out.println(cars);
+    cars.removeLast();
+    System.out.println(cars);
+    System.out.println(cars.getFirst());
+    System.out.println(cars.getLast());
+  }
+}
