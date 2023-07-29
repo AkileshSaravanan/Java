52279e227551c63ffe3ead01c689f585c5c9132c
AKILESH S <rmsakilesh23@gmail.com>
2023-07-29 18:04:15 +0530

    initial commit

---
+++ Practice/Practice - 3/OOPS/HashSet/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    System.out.println(cars);
+    System.out.println(cars.contains("Mazda"));
+    cars.remove("Volvo");
+    System.out.println(cars);
+    System.out.println(cars.size());
+    cars.clear();
+    System.out.println(cars);
+  }
+}
