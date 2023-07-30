1d3d88f8a5e926a23bc8cc3a19f26dacc4bd12b0
AKILESH S <rmsakilesh23@gmail.com>
2023-07-31 00:44:01 +0530

    initial commit

--- Practice/Practice - 3/Examples/ReverseString.java
+++ Practice/Practice - 3/Examples/ReverseString.java
@@ -1,5 +1,6 @@
-public class ReverseString{
+/*public class ReverseString{
     public static void main(String[] args) {
+      //Scanner s = new Scanner(System.in);
       String originalStr = "Hello";
       String reversedStr = "";
       System.out.println("Original string: " + originalStr);    
@@ -9,5 +10,15 @@ public class ReverseString{
       System.out.println("Reversed string: "+ reversedStr);
     }
   }
-  
-  
+*/
+import java.util.Scanner;
+class ReverseString{
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the String:");
+    String s=in.nextLine();
+    for(int i=s.length()-1;i>=0;i--){
+      System.out.print(s.charAt(i));
+    }
+  }
+}
