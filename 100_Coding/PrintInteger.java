53094dd067b75d818af0bc86c85f3be0f65b3c27
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-02 11:27:35 +0530

    initial commit

---
+++ 100_Coding/PrintInteger.java
@@ -0,0 +1,11 @@
+//Print Integer in java
+public class PrintInteger{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
