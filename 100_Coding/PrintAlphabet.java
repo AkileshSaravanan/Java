9f579e1dc9b679434ada3f5c7b22c6b4040c59a9
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-04 21:12:32 +0530

    initial commit

---
+++ 100_Coding/PrintAlphabet.java
@@ -0,0 +1,13 @@
+//Print all alphabet using for loop Program in java
+import java.util.Scanner;
+public class PrintAlphabet
+{
+    public static void main(String args[])
+    {
+        char ch;
+        for(ch = 'a';ch <='z';ch++)
+        {
+            System.out.println(ch);
+        }
+    }
+}
