85997df597a1c6ad3d5512c21f163026975b76ff
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-04 21:16:32 +0530

    initial commit

---
+++ 100_Coding/EnchancedLoop.java
@@ -0,0 +1,12 @@
+//Enhance loop in java Program
+public class EnchancedLoop
+{
+    public static void main(String args[])
+    {
+        int primes[] = {1,3,5,7,11,13,17,19,23,29};
+        for(int i : primes)
+        {
+            System.out.println(i);
+        }
+    }
+}
