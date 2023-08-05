5ea3fa98f1db01e1e87670edaee36168b358597c
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-05 21:03:53 +0530

    initial commit

---
+++ 100_Coding/AddTwoMatrix.java
@@ -0,0 +1,46 @@
+//How to add two matrix in java Program
+import java.util.Scanner;
+public class AddTwoMatrix
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the no of rows:");
+        int m = s.nextInt();
+        System.out.println("Enter the no of columns");
+        int n = s.nextInt();
+        int first[][] = new int [m][n];
+        int second[][] = new int [m][n];
+        int sum[][] = new int [m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                first[i][j] = s.nextInt(); 
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                second[i][j] = s.nextInt(); 
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                sum[i][j] = first[i][j] + second [i][j]; 
+            }
+        }
+        System.out.println("The Sum of Two Matric is :");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(sum[i][j]+"\t"); 
+            }
+            System.out.println();
+        }
+    }
+}
