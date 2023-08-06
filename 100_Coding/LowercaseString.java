47830e662dad5cc414beb9a59024aaf2e3fc36a7
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-06 21:55:21 +0530

    initial commit

---
+++ 100_Coding/LowercaseString.java
@@ -0,0 +1,10 @@
+//How to convert all char in string lower case in java Program
+public class LowercaseString
+{
+    public static void main(String args[])
+    {
+    String s1="TECHNOLOGY by Akileshsaravanan HELLO stRIng"; 
+    String s1lower=s1.toLowerCase();
+    System.out.println(s1lower);
+    }
+}
