b1af01395198e8462aaddb26253d131237bd38b6
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-06 21:52:59 +0530

    initial commit

---
+++ 100_Coding/RemoveSpaceString.java
@@ -0,0 +1,10 @@
+//How to remove space in string both end in java Program
+public class RemoveSpaceString
+{
+    public static void main(String args[])
+    { 
+        String s1=" hello string ";
+        System.out.println(s1+"Technolamror");
+        System.out.println(s1.trim()+"Technolamror");
+    }
+}
