09e3f57025db286f85708293bc13f7889325dac5
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-06 21:22:10 +0530

    initial commit

--- 100_Coding/CompareTwoString.java
+++ 100_Coding/CompareTwoString.java
@@ -18,3 +18,9 @@ public class CompareTwoString
         System.out.println(s1.compareTo(s4));
     }
 }
+/*
+ * Output
+ * The method returns 0 if the string is equal to the other string.
+ * A value less than 0 is returned if the string is less than the other string (less characters) and 
+ * a value greater than 0 if the string is greater than the other string (more characters).
+ */
