43bbfe627a19945e0cd530b04e29e0956fbd236c
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-06 21:51:14 +0530

    initial commit

---
+++ 100_Coding/SplitString.java
@@ -0,0 +1,13 @@
+//How to split string in java Program
+public class SplitString
+{
+    public static void main(String args[])
+    {
+    String s1="java string split method by Technolamror";
+    String[] words=s1.split("\\s");
+    for(String w:words)
+    { 
+        System.out.println(w);
+    }
+    }
+}
