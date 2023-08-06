1e76353a2da5f7ec81186d2d95a52eede489fd0a
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-06 21:47:09 +0530

    initial commit

---
+++ 100_Coding/IndexOfString.java
@@ -0,0 +1,15 @@
+//How to use indesOf() in java Program
+public class IndexOfString
+{
+    public static void main(String args[])
+    { 
+        String s1="this is index of example";
+        int index1=s1.indexOf("is");
+        int index2=s1.indexOf("index");
+        System.out.println(index1+"\n"+index2);
+        int index3=s1.indexOf("is",4);
+        System.out.println(index3);
+        int index4=s1.indexOf('s'); 
+        System.out.println(index4);
+    }
+}
