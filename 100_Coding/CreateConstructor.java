9e861637b554f8cff795069c5669662f3eab4572
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-07 22:50:57 +0530

    initial commit

---
+++ 100_Coding/CreateConstructor.java
@@ -0,0 +1,13 @@
+//How to create constructor in java Program
+public class CreateConstructor 
+{
+    //constructor method
+    CreateConstructor() 
+    {
+        System.out.println("Constructor method called.");
+    }
+    public static void main(String[] args) 
+    {
+        CreateConstructor object = new CreateConstructor(); //creating object
+    }
+}
