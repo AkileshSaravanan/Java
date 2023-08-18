9dac987ad1aef80201ef263e01d1d57cfe600be5
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-18 12:04:35 +0530

    initial commit

---
+++ 100_Coding/WriteData.java
@@ -0,0 +1,19 @@
+//How to write data in text file using java program
+import java.io.FileOutputStream;
+public class WriteData
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            FileOutputStream fout=new FileOutputStream("D:\\testout.txt"); 
+            fout.write(65);
+            fout.close();
+            System.out.println("success.. by Technolamror.");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
