d5bf7b0d34b62416ccabec4e25cb01908ced1653
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-18 12:29:27 +0530

    initial commit

--- 100_Coding/GetURL.java
+++ 100_Coding/GetURL.java
@@ -7,7 +7,7 @@ public class GetURL
     {
         try
         {
-            URL url=new URL("https://github.com/AkileshSaravanan");
+            URL url=new URL("https://www.linkedin.com/in/Akilesh--S");
             System.out.println("Protocol: "+url.getProtocol()); 
             System.out.println("Host Name: "+url.getHost()); 
             System.out.println("Port Number: "+url.getPort()); 
