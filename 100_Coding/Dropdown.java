f2da66cbcea16e998a98a11415a006882b49a89b
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-19 20:43:33 +0530

    initial commit

--- 100_Coding/Dropdown.java
+++ 100_Coding/Dropdown.java
@@ -7,10 +7,10 @@ public class Dropdown
         Frame f= new Frame(); 
         Choice c=new Choice(); 
         c.setBounds(100,100, 75,75);
-        c.add("Item 1 by Akilesh"); 
-        c.add("Item 2 by Saravanan"); 
-        c.add("Item 3 by Marimuthu"); 
-        c.add("Item 4 by Ramasamy");
+        c.add("Item 1 by RMSA"); 
+        c.add("Item 2 by RMS"); 
+        c.add("Item 3 by RM"); 
+        c.add("Item 4 by NR");
         c.add("Item 5 by God"); 
         f.add(c); 
         f.setSize(400,400); 
