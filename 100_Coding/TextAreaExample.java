ac76d6dcecb462f5893f6ebb7937ba22c7c9c66e
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-19 20:37:28 +0530

    initial commit

--- 100_Coding/TextAreaExample.java
+++ 100_Coding/TextAreaExample.java
@@ -1,19 +1,19 @@
 //How to add text area program in java
 import java.awt.*;
-public class TextArea
+public class TextAreaExample
 {
-    TextArea()
+    TextAreaExample()
     {
         Frame f= new Frame();
         TextArea area=new TextArea("Welcome"); 
         area.setBounds(10,30, 300,300);
         f.add(area); 
-        f.setSize(400,400);
+        f.setSize(400,400); 
         f.setLayout(null); 
         f.setVisible(true);
     }
-    public static void main(String args[])
-    {
-        new TextArea();
-    }
+public static void main(String args[])
+{
+    new TextAreaExample();
+}
 }
