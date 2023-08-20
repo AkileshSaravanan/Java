912b761fb9a5f7fa90830c86e8f5386f085dfd2f
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-20 12:37:17 +0530

    initial commit

---
+++ 100_Coding/StringToLong.java
@@ -0,0 +1,10 @@
+//How to convert string to long in java
+public class StringToLong
+{
+    public static void main(String args[])
+    { 
+        String s="9990449935";
+        long l=Long.parseLong(s); 
+        System.out.println(l);
+    }
+}
