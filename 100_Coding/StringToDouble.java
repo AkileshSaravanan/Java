de07e419cc49838435c043278a26cfaaf04e4451
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-20 12:40:10 +0530

    initial commit

---
+++ 100_Coding/StringToDouble.java
@@ -0,0 +1,10 @@
+//How to convert string to double in java program
+public class StringToDouble
+{
+    public static void main(String args[])
+    {
+        String s="23.6";
+        double d=Double.parseDouble("23.6"); 
+        System.out.println(d);
+    }
+}
