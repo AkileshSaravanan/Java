0868e8cc281eae97ccfd795f58ab72a3ecef4e15
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-20 12:39:03 +0530

    initial commit

---
+++ 100_Coding/StringToFloat.java
@@ -0,0 +1,10 @@
+//How to convert string to float in java
+public class StringToFloat
+{ 
+    public static void main(String args[])
+    { 
+        String s="23.6";
+        float f=Float.parseFloat("23.6"); 
+        System.out.println(f);
+    }
+}
