51b05619ae431e54b0245bf26be8b06fff0cc22a
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-21 21:55:43 +0530

    initial commit

---
+++ 100_Coding/HashSets.java
@@ -0,0 +1,20 @@
+//How to create Hashset program in java
+import java.util.*;
+class HashSets
+{
+    public static void main(String args[])
+    {
+    //Creating HashSet and adding elements 
+    HashSet<String> set=new HashSet<String>(); 
+    set.add("RMSA");
+    set.add("RMS");
+    set.add("RM"); 
+    set.add("KNR");
+    //Traversing elements Iterator<String> 
+    itr=set.iterator(); 
+    while(itr.hasNext())
+    { 
+        System.out.println(itr.next());
+    }
+    }
+}
