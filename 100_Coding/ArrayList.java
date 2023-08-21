da187d6b67e2dc5b1d082a20c1ba4b73bc8b85f4
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-21 21:33:04 +0530

    initial commit

---
+++ 100_Coding/ArrayList.java
@@ -0,0 +1,18 @@
+//Create ArrayList program in java
+import java.util.*; 
+class ArryaList
+{
+    public static void main(String args[]){
+    ArrayList<String> list=new ArrayList<String>();//Creating arraylist 
+    list.add("RMSA");//Adding object in arraylist 
+    list.add("RMS");
+    list.add("RM");
+    list.add("KNR");
+    //Traversing list through Iterator 
+    Iterator itr=list.iterator(); 
+    while(itr.hasNext())
+    { 
+        System.out.println(itr.next());
+    }
+    }
+}
