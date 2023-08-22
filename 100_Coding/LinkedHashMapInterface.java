d68f9b494f1ddcd6879a5ea3e514f47a5c455537
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-22 22:28:39 +0530

    initial commit

--- 100_Coding/LinkedHashMapInterface.java
+++ 100_Coding/LinkedHashMapInterface.java
@@ -5,9 +5,9 @@ class LinkedHashMapInterface
     public static void main(String args[])
     {
         LinkedHashMap<Integer,String> hm=new LinkedHashMap<Integer,String>(); 
-        hm.put(100,"Rajendra");
-        hm.put(101,"Vijay");
-        hm.put(102,"Technolamror");
+        hm.put(100,"RMSA");
+        hm.put(101,"RMS");
+        hm.put(102,"RM");
         for(Map.Entry m:hm.entrySet())
         { 
             System.out.println(m.getKey()+" "+m.getValue());
