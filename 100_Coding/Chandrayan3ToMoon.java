c9e1ae246d0ad880c51e2deb8e40831c2079a130
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-23 20:37:34 +0530

    initial commit

---
+++ 100_Coding/Chandrayan3ToMoon.java
@@ -0,0 +1,18 @@
+public class Chandrayaan3ToMoon 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("🇮🇳🚀🌕 Chandrayaan 3: \"Hello, Moon!\"");
+        System.out.println("=======================================");
+        System.out.println("|         🛰️ Chandrayaan 3 Speaking...        |");
+        System.out.println("|       🌌 \"I've Landed Safely on You!\"       |");
+        System.out.println("|    🇮🇳 India's Message to the Cosmos!    |");
+        System.out.println("|           🌠 Let's Explore Together!         |");
+        System.out.println("|            👋 Sending Moon Vibes...          |");
+        System.out.println("|               🚩 Jai Hind! 🚩          |");
+        System.out.println("|        🎉 Congratulations, ISRO! 🎉    |");
+        System.out.println("|              🚀 Proud Moment! 🚀             |");
+        System.out.println("=======================================");
+        System.out.println("#Chandrayaan3 #HelloMoon #SpaceExploration");
+    }
+}
