032d2b03656a7b0287d600ec602da6cbb64dfe65
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-28 14:27:58 +0530

    initial commit

---
+++ Practice/Practice-4/Basic/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Factorial
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int fact = 1;
+        System.out.print("Enter the No of Factorial: ");
+        int n = s.nextInt();
+        for(int i =0;i<=n;i++)
+        {
+            fact = fact *i;
+        }
+        System.out.println("Factorial of "+ n +" is"+fact);
+    }
+}
