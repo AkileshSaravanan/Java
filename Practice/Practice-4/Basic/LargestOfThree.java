44d92de11d0ead9761885aeaf0aa85ac882b1eb5
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-29 15:53:41 +0530

    initial commit

--- Practice/Practice-4/Basic/LargestOfThree.java
+++ Practice/Practice-4/Basic/LargestOfThree.java
@@ -9,6 +9,7 @@ public class LargestOfThree
         System.out.print("Enter the Second Number: ");
         int b = s.nextInt();  
         System.out.print("Enter the Third Number: ");
+        int c = s.nextInt();
         int temp = a > b ? a : b;
         int result = c > temp ? c : temp;
         System.out.println("Largest Number is: "+ result);
