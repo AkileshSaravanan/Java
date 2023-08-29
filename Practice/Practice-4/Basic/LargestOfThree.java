e468f0a79d374c27d3fae16e680d263aae36612d
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-29 15:53:07 +0530

    initial commit

---
+++ Practice/Practice-4/Basic/LargestOfThree.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LargestOfThree
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the First Number: ");
+        int a = s.nextInt(); 
+        System.out.print("Enter the Second Number: ");
+        int b = s.nextInt();  
+        System.out.print("Enter the Third Number: ");
+        int temp = a > b ? a : b;
+        int result = c > temp ? c : temp;
+        System.out.println("Largest Number is: "+ result);
+    }
+}
