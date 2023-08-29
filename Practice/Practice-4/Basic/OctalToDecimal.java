bc8ca15f048a5436f2c18116f05fc41daaf49700
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-29 16:25:38 +0530

    initial commit

--- Practice/Practice-4/Basic/OctalToDecimal.java
+++ Practice/Practice-4/Basic/OctalToDecimal.java
@@ -4,7 +4,7 @@ public class OctalToDecimal
     public static void main(String args[])
     {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter The String:");
+        System.out.print("Enter The String: ");
         String octal = s.nextLine();
         int decimal = Integer.parseInt(octal,8);
         System.out.println("Converted decimal number of "+ octal + " is : "+decimal);
