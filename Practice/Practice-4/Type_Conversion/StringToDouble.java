fbbfaad18f99ee64b83fe346c7fa37a7814527c5
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 21:50:04 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/StringToDouble.java
@@ -0,0 +1,9 @@
+public class StringToDouble
+{
+    public static void main(String args[])
+    {
+        String s = "23.6";
+        double i = Double.parseDouble(s);
+        System.out.println(i);
+    }
+}
