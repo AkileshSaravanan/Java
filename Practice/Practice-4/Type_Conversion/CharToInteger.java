eb9a657f3f7845840118c0e19e61ed0a2753e59e
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 12:34:02 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/CharToInteger.java
@@ -0,0 +1,9 @@
+public class CharToInteger
+{
+    public static void main(String args[])
+    {
+        char c = '1';
+        int a = Integer.pareseInt(String.valurOf(c));
+        System.out.println(a);
+    }
+}
