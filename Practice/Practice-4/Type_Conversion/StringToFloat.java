3a9d612e466147752be7de72fe7c967a8c4acfdb
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 21:48:31 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/StringToFloat.java
@@ -0,0 +1,9 @@
+public class StringTofloat
+{
+    public static void main(String args[])
+    {
+        String s = "23.6";
+        float i = Float.parseFloat(s);
+        System.out.println(i);
+    }
+}
