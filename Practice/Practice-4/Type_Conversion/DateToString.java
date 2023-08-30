9f0ce5af0dbe2c2b044e1a883771ee2216ba124b
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 22:08:23 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/DateToString.java
@@ -0,0 +1,14 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+public class DateToString
+{
+    public static void main(String args[])
+    {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy0M-d hh:mm:ss");
+        String strDate = dateFormat.format(date);
+        System.out.println("Converted String: " + strDate);
+    }
+}
