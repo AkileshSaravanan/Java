de9b1f19941edc49a4a144297162c94484973518
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 12:41:41 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/LongToString.java
@@ -0,0 +1,9 @@
+public class LongToString
+{
+    public static void main(String args[])
+    {
+        long i = 99939399L;
+        String s = Long.toString(i);
+        System.out.println(s);
+    }
+}
