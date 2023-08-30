ff90759a3fb931d236dfddea48ae6b7fd86965de
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 12:26:13 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/ChatToInt.java
@@ -0,0 +1,12 @@
+public class ChatToInt
+{
+    public static void main(String args[])
+    {
+        char c = 'a';
+        char c1 = '1';
+        int a = c;
+        int b = c1;
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
