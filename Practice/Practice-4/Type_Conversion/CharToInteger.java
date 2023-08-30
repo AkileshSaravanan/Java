79197fbb6f517fd1323efc580338ea760efc01fb
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 12:34:46 +0530

    initial commit

--- Practice/Practice-4/Type_Conversion/CharToInteger.java
+++ Practice/Practice-4/Type_Conversion/CharToInteger.java
@@ -3,7 +3,7 @@ public class CharToInteger
     public static void main(String args[])
     {
         char c = '1';
-        int a = Integer.pareseInt(String.valueOf(c));
+        int a = Integer.parseInt(String.valueOf(c));
         System.out.println(a);
     }
 }
