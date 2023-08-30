a96e6691d6d43f11ee1adf75e0e8322a871f85fc
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 21:51:39 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/DoubleToString.java
@@ -0,0 +1,11 @@
+public class DoubleToString
+{
+    public static void main(String args[])
+    {
+        double d = 23.6;
+        String s1  = Float.toString(d);
+        String s2 = String.valueOf(d);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
