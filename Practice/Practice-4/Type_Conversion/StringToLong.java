901881a8056eefbd81f0e47a422d3d8e61e72c8f
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-08-30 12:38:23 +0530

    initial commit

---
+++ Practice/Practice-4/Type_Conversion/StringToLong.java
@@ -0,0 +1,9 @@
+public class StringToInt
+{
+    public static void main(String args[])
+    {
+        String s = "9990449935";
+        int i = Long.parseLong(s);
+        System.out.println(i);
+    }
+}
