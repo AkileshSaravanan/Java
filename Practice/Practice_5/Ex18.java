daa9987cad7311a69a821bdebbd378714134488f
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-09-17 21:47:28 +0530

    initial commit

--- Practice/Practice_5/Ex18.java
+++ Practice/Practice_5/Ex18.java
@@ -30,7 +30,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 public class Ex18
 {
 	public static void main (String[] args) throws java.lang.Exception
