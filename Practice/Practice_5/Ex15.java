38d2df57dde734a95bff6483f0881be83e91a7ae
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-09-17 21:27:25 +0530

    initial commit

--- Practice/Practice_5/Ex15.java
+++ Practice/Practice_5/Ex15.java
@@ -32,8 +32,7 @@ import java.util.*;
 import java.lang.Math;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+public class Ex15
 {
 	public static void main (String[] args){
 	    Scanner sc=new Scanner(System.in);
