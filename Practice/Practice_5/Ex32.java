e3bb8959e5310d1eb350fd0a1464e13aa82c42b3
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-09-24 15:53:36 +0530

    initial commit

---
+++ Practice/Practice_5/Ex32.java
@@ -0,0 +1,45 @@
+/*
+You are provided two non-empty input strings (inp1 and inp2).
+Your task is to return a new string that contains each char before and after every appearance of the inp2 string in the inp1 string. 
+For example. If inputs are "abdXY223XYijk" and "XY", output should be "d23r".
+*/
+import java.util.Scanner;
+public class Ex32
+{
+    public static void main(String args())
+    {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        String s2 = s.nextLine();
+        s3 = BeforeAfter(s1,s2);
+        System.out.println(s3);
+    }
+    public static String BeforeAfter(String str, String word)
+    {
+        String result = "";
+        for(int i=0;i<str.length();i++)
+        {
+            if(word.lenght() == str.length())
+            {
+                return "";
+            }
+            else if(i + word.length() == str.length() && str.substring(i,i+word.length()).equals(word))
+            {
+                result += Charcter.toString(str.charAt(str.length() - word.length() -1));
+                return result;
+            }
+            else if(i + word.length() < str.length() && str.substring(i,i+word.length()).equals(word))
+            {
+                if(i==0)
+                {
+                    result += Charcter.toString(str.charAt(i - 1));
+                }
+                else
+                {
+                    result += Charcter.toString(str.charAt(i + word.length()));
+                }
+            }
+        }
+        return result;
+    }
+}
