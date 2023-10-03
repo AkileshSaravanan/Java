4043d586cd90e84d5607661c3696966c9095af4d
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-03 11:09:17 +0530

    initial commit

--- Practice/Practice_5/Ex33.java
+++ Practice/Practice_5/Ex33.java
@@ -4,7 +4,7 @@ Based on user submissions during the contest, the system calculates and assigns 
 Ideally, it is recommended that users practice problems in increasing order of difficulty.
 Our Chef has some students in his coding class who are practicing problems. 
 Given the difficulty of the problems that the students have solved in order, help the Chef identify if they are solving them in non-decreasing order of difficulty. 
-Output “Yes” if the problems are attempted in non-decreasing order of difficulty rating and “No” if not.
+Output "Yes" if the problems are attempted in non-decreasing order of difficulty rating and "No" if not.
 
 Input Format
 The first line of input will contain a single integer T, denoting the number of test cases. The description of the test cases follows.
@@ -12,7 +12,8 @@ Each test case consists of 2 lines of input.
 The first line contains a single integer N, the number of problems solved by the students
 The second line contains N space-separate integers, the difficulty ratings of the problems attempted by the students in order.
 Output Format
-For each test case, output in a new line “Yes” if the problems are attempted in non-decreasing order of difficulty rating and “No” if not. The output should be printed without the quotes.
+For each test case, output in a new line "Yes" if the problems are attempted in non-decreasing order of difficulty rating and "No" if not. 
+The output should be printed without the quotes.
 Each letter of the output may be printed in either lowercase or uppercase. For example, the strings yes, YeS, and YES will all be treated as equivalent.
 
 Constraints
@@ -38,9 +39,10 @@ Yes
 No
 No
 */
+
 import java.util.Scanner;
 
-public class Main {
+public class Ex33 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
