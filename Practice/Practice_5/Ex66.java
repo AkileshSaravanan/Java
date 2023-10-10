a93a108cc54168e5e4fb6c5754548bbd50e03a90
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:15:10 +0530

    initial commit

---
+++ Practice/Practice_5/Ex66.java
@@ -0,0 +1,18 @@
+/*
+We can use "println" to have the next outputs be on the next line.
+It is like pressing the "Enter" / "Return" key on your keyboard, but for the output displayed.
+
+Task
+Write a program which does the following
+
+Output: 3 + 4
+Output: 2 + 1 in a separate line using the given command.
+*/
+public class Ex66
+{
+	public static void main (String[] args)
+	{
+		System.out.println(3 + 4);
+		System.out.print(2 + 1);
+	}
+}
