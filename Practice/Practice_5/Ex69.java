9b5ae5910dfffd4a6fa2924bdb893cc4214e8f7b
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:19:25 +0530

    initial commit

---
+++ Practice/Practice_5/Ex69.java
@@ -0,0 +1,27 @@
+/*
+Write a program which does the following
+
+Output the result - Product of the integers 
+8
+8 & 
+9
+9
+Output the result - Sum of 
+100
+100 & 
+200
+200 multiplied by 
+2
+2
+Output both results to separate lines
+*/
+public class Ex69
+{
+	public static void main (String[] args)
+	{
+	    // Update your code here
+	    System.out.println((8*9));
+	    System.out.println((100+200)*2);
+
+	}
+}
