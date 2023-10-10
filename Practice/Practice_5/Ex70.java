f43d4b3cf3369d48c04356866b289fe94e17c436
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:21:36 +0530

    initial commit

---
+++ Practice/Practice_5/Ex70.java
@@ -0,0 +1,15 @@
+/*
+Write a program which does the following
+
+Output the result - Difference of the integers 9 & 2
+Output the result - Division of 30 by 5
+Output both results to the console on separate lines
+*/
+public class Ex70
+{
+	public static void main (String[] args)
+	{
+		System.out.println(9 - 2);
+		System.out.println(30 / 5);
+	}
+}
