ee2258d29310e0808fa4e984aa865e56ead22321
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:16:01 +0530

    initial commit

---
+++ Practice/Practice_5/Ex67.java
@@ -0,0 +1,19 @@
+/*
+Write a program for the following problem
+
+Let’s consider a rectangle of sides 11 and 13.
+Output the following on separate lines
+Area of the rectangle
+Perimeter of the rectangle
+*/
+public class Ex67
+{
+	public static void main (String[] args)
+	{
+	    // area - length * breadth 
+		System.out.println(11 * 13);
+		
+		//perimeter - 2(length + breadth)
+		System.out.print(2 * (11 + 13));
+	}
+}
