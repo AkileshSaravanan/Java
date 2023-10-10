e0317b01a48e9ec83f35533d4642b047dfcfb313
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:09:14 +0530

    initial commit

---
+++ Practice/Practice_5/Ex64.java
@@ -0,0 +1,15 @@
+/*
+We can also perform mathematical operations with the print statement.
+Try printing the sum of 21 and 40.
+
+Notice that on the editor on the right, the instructions appear in gray color.
+This is because they are comments and don't affect the code. The way we write comments is by adding "//" before it.
+*/
+public class Ex64
+{
+	public static void main (String[] args)
+	{
+	    // Enter 21 + 40 inside the print statement
+		System.out.print(21+40);
+	}
+}
