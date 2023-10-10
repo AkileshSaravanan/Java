b1fa88e70f5e04c150366baa9e22ffef27d644cf
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:14:14 +0530

    initial commit

---
+++ Practice/Practice_5/Ex65.java
@@ -0,0 +1,18 @@
+/*
+We can add as many print statements as we want.
+Write a program which does the following
+
+Output: 3 + 4
+Output: 2 + 1.
+Note - Notice that in your output they are printed together without any space between them.
+*/
+public class Ex65
+{
+	public static void main (String[] args)
+	{
+		// update your code here
+		System.out.print(3+4);
+		System.out.print(2+1);
+		
+	}
+}
