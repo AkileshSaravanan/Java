7dfff781579a0d7fbd48fb1dbdb84d1fa96d75ec
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:06:55 +0530

    initial commit

---
+++ Practice/Practice_5/Ex62.java
@@ -0,0 +1,20 @@
+/*
+Let us output an integer in Java.
+
+Java has a few lines of code which we write in almost all programs.
+These are the first 3 lines that you see in the program to your right. (Ignore what those lines mean - we will explain them later)
+
+What matters for now is just Line 6:
+System.out.println(12);
+This is what we use to output/print anything to the screen.
+Every Java statement ends with a semicolon (;)
+This code just prints 12 on the screen when you run it.
+*/
+public class Ex62
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		System.out.println(12);
+	}
+}
