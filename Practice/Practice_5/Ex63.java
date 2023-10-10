21aece8d3c660338929512d584ad5b8f83683f04
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-10 11:08:14 +0530

    initial commit

---
+++ Practice/Practice_5/Ex63.java
@@ -0,0 +1,16 @@
+/*
+Let's see how to output some text.
+There is only one difference with printing a number: all textual content should start and end with double quotes.
+For eg -
+
+System.out.println("Chef");
+Task
+Replace the _ (underscores) in the editor with the text "I love Java" to output "I love Java".
+*/
+public class Ex63
+{
+	public static void main (String[] args)
+	{
+		System.out.println("I love Java");
+	}
+}
