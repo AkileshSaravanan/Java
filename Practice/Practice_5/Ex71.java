d6154a1cb9c8a6d3338cf6865b496d181bf391a8
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 12:54:57 +0530

    initial commit

---
+++ Practice/Practice_5/Ex71.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ex71
+{
+	public static void main (String[] args)
+	{
+	    int length = 11;
+        int width = 13;
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+        width = 15; 
+        System.out.println(length * width);
+        System.out.println(2 * (length + width));
+	}
+}
