ca9becfb00216a0b50c6e675ed1fa9e4ad1d81af
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 13:11:38 +0530

    initial commit

---
+++ Practice/Practice_5/Ex78.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ex78
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+        int b = 23;
+        int r = 45;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r == b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+        b = 15;
+        r = 15;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r == b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+    }
+}
