e57a4ae50ab7db831aadac60510d4b8a0eb13d80
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 13:03:06 +0530

    initial commit

---
+++ Practice/Practice_5/Ex75.java
@@ -0,0 +1,11 @@
+public class Ex75
+{
+	public static void main (String[] args)
+	{
+		String word = "Programming";
+        // Update your code below this line
+        System.out.println(word.charAt(2));
+        System.out.println(word.charAt(1));
+
+	}
+}
