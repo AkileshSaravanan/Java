6c0218bb8cec42857fac298febc6f17af5fc5949
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 13:23:17 +0530

    initial commit

---
+++ Practice/Practice_5/Ex82.java
@@ -0,0 +1,14 @@
+public class Ex82
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int i = 0;
+		do{
+		    System.out.println(i);
+		    i++;
+		}
+		while(i < 6);
+		
+	}
+}
