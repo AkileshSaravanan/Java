5b2dc305fc41132d59fc41de608a546b113e87d3
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 13:02:05 +0530

    initial commit

---
+++ Practice/Practice_5/Ex74.java
@@ -0,0 +1,8 @@
+public class Ex74
+{
+	public static void main (String[] args)
+	{
+        String txt = "NumeroTres";
+        System.out.println("The length of the word is: " + txt.length());
+	}
+}
