288e8392ece091c3a2eeb4becda4d5ccff254a5e
AkileshSaravanan <rmsakilesh23@gmail.com>
2023-10-11 13:00:27 +0530

    initial commit

---
+++ Practice/Practice_5/Ex73.java
@@ -0,0 +1,10 @@
+public class Ex73
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String num1 = "25";
+        String num2 = "69";
+        System.out.print(num1 + num2);
+	}
+}
